217215adb40c6e73a392a6242d9c776dc78a6e8d
Omaigodi <caina.zampieri@gmail.com>
2022-03-02 02:18:28 -0300

    first commit

---
+++ contracts/MemoryToken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MemoryToken is ERC721URIStorage {
+    
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Memory Token", "MTT") {}
+
+    function mint(address _wallet, string memory _tokenURI) public returns(uint256){
+
+        uint256 tokenId = _tokenIdCounter.current();
+
+        _safeMint(_wallet, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
+        _tokenIdCounter.increment();
+
+        return tokenId;
+
+    }
+
+}
---
+++ test/MemoryTokenTest.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai')
+
+const MemoryToken = artifacts.require('./contratcs/MemoryToken.sol')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('Memory Token', (accounts) => {
+
+    let token        
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+
+            token = await MemoryToken.deployed()
+
+            const address = token.address
+
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        }) 
+        it('has a name', async () => {
+            const name = await token.name()
+            assert.equal(name, 'Memory Token')
+        }) 
+        it('has a symbol', async () => {
+            const name = await token.symbol()
+            assert.equal(name, 'MTT')
+        }) 
+    })
+
+})
